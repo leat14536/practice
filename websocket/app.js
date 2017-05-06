8e40368065458d4cc5cf86eaf860367119274378
zltyiu@qq.com <zltyiu@qq.com>
2017-05-06 18:51:38 +0800

    5.6

--- websocket/app.js
+++ websocket/app.js
@@ -1,22 +1,18 @@
 /**
  * Created by Administrator on 2017/5/5 0005.
  */
-var bodyParser = require('body-parser');
-var swig = require('swig');
 var express = require('express');
 var app = express();
 
-var server = app.listen(3000);
+var server = app.listen(8000,()=>{
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
 var io = require('socket.io')(server);
 
-
-app.use( bodyParser.urlencoded({extended:true}) );
-
-app.engine('html', swig.renderFile);
-app.set('views','./views');
-app.set('view engine','html');
-
-
+app.use(express.static('views'));
 
 app.get('/', function(req,res){
     res.render('index')
@@ -25,14 +21,14 @@ app.get('/', function(req,res){
 io.on('connection', function(client){
     var i=0;
 
-    client.on('send',(data)=>{
+    client.on('message',(data)=>{                   //服务器message事件, 客户端使用emit('message',val)时触发,参数为emit的第二个参数
         setTimeout(()=>{
             i++;
-            client.emit('message',i+' : '+data);
+            client.emit('message',i+' : '+data);    //向客户端发送消息,客户端on('message')接收
         },3000);
     })
 
-    client.on('disconnect', function(){
+    client.on('disconnect', function(){             //客户端使用socket.close();时触发,关闭连接
         console.log('disconnect')
     });
 });
