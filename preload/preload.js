05d3a7131cbfee4a6a73e722b5ba52cc7c73176b
zltyiu@qq.com <zltyiu@qq.com>
2017-05-13 22:06:43 +0800

    图片预加载

--- preload/preload.js
+++ preload/preload.js
@@ -8,15 +8,20 @@
         this.imgs = (typeof imgs==='string') ? [imgs]:imgs;
         this.opts = $.extend({},preload.DEFAULTS,options);
 
-        this._unoredered();
+        if(this.opts.order === 'ordered') {
+            this._oredered();
+        }else{
+            this._unoredered();
+        }
     }
 
     preload.DEFAULTS = {
+        order: 'unordered',
         each: null,             //每一张加载完后执行
         all: null               //所有图片加载完后执行
     }
 
-    preload.prototype._unoredered = function(){
+    preload.prototype._unoredered = function(){ //无序加载
         let imgs = this.imgs;
         let opts = this.opts;
         let count = 0;
@@ -37,6 +42,31 @@
 
             imgObj.src = imgs[i];
         })
+    }
+
+    preload.prototype._oredered = function(){   //有序加载
+        let imgs = this.imgs;
+        let opts = this.opts;
+        let count = 0;
+        let len = imgs.length;
+
+        load();
+        //有序加载
+        function load(){
+            var imgObj = new Image();
+            $(imgObj).on('load error', ()=>{
+                opts.each && opts.each(count);
+                if(count>=len){
+                    //全部加载完成
+                    opts.all && opts.all();
+                }else{
+                    load();
+                }
+                count++;
+            })
+
+            imgObj.src = imgs[count];
+        }
     }
 
     $.extend({
