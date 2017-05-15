cd265bd99cad7c6f7bf83dc41ba46715bc5247b8
zltyiu@qq.com <zltyiu@qq.com>
2017-05-15 22:08:59 +0800

    loading

--- css3loading/webpack.config.js
+++ css3loading/webpack.config.js
@@ -4,6 +4,8 @@
 let path = require('path')
 let ExtractTextPlugin = require('extract-text-webpack-plugin'); //分离css文件
 let HtmlWebpackPlugin = require('html-webpack-plugin');         //生成html文件
+let autoprefixer = require('autoprefixer');
+let webpack = require('webpack');
 
 module.exports = {
     entry: './main.js',
@@ -15,19 +17,27 @@ module.exports = {
         rules: [{
             test: /\.css$/,
             use: ExtractTextPlugin.extract({
-                use: 'css-loader'
+                use:"css-loader!postcss-loader"
             })
         }]
     },
     plugins: [
         new ExtractTextPlugin('styles.css'),    //分离css文件
         new HtmlWebpackPlugin({
-            filename: 'index.html',              //模板
+            filename: 'test.html',              //模板
             template: 'index.html',             //文件名
             minify: {
                 removeComments: true,           //去除注释
                 collapseWhitespace: true,       //去除空格
             }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options:{
+                postcss:function(){
+                    return[autoprefixer({ browsers: ['not ie <= 8'] })]
+                }
+            }
         })
-    ]
+    ],
+
 };
