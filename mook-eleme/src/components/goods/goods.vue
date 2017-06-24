7156b45b79a71315434592191d4282f79e87aa23
zltyiu@qq.com <zltyiu@qq.com>
2017-06-24 22:05:32 +0800

    完善goods,shopCar

--- mook-eleme/src/components/goods/goods.vue
+++ mook-eleme/src/components/goods/goods.vue
@@ -1,29 +1,261 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
-    <div class="foods-wrapper"></div>
+    <div class="menu-wrapper" ref="menu-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods"
+            class="menu-item"
+            @click = "selectMenu(index,$event)"
+            :class="{'current':currentIndex===index}">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"  ref="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list food-list-hook">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <shopCart :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopCart>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll';
+  import shopCart from '@/components/shopCart/shopCart';
+
+  const ERR_OK = 0;
+
   export default{
-      name: 'goods'
+    name: 'goods',
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      };
+    },
+    computed: {
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+
+          if (!height2 || this.scrollY >= height1 && this.scrollY < height2) {
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+
+      this.$http({
+        url: '/goods',
+        ctx: this,
+        success (data) {
+          if (data.errno === ERR_OK) {
+            this.goods = data.data;
+            this.$nextTick(() => {
+              this._initScroll();
+              this._calculateHeight();
+            });
+          }
+        }
+      });
+    },
+    methods: {
+      _initScroll () {
+        this.menuScroll = new BScroll(this.$refs['menu-wrapper'], {
+          click: true
+        });
+
+        this.foodsScroll = new BScroll(this.$refs['foods-wrapper'], {
+          probeType: 3
+        });
+
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight () {
+        let foodList = this.$refs['foods-wrapper'].querySelectorAll('.food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      selectMenu (index, e) {
+        if (!e._constructed) return;
+        let foodList = this.$refs['foods-wrapper'].querySelectorAll('.food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      }
+    },
+    components: {
+      shopCart
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-  .goods{
+  @import "../../common/styles/mixin";
+
+  .goods {
     display: flex;
     position: absolute;
     top: 174px;
     bottom: 46px;
     width: 100%;
     overflow: hidden;
-    .menu-wrapper{
-
+    .menu-wrapper {
+      flex: 0 0 80px;
+      width: 80px;
+      background-color: #f3f5f7;
+      .menu-item {
+        display: table;
+        height: 54px;
+        width: 56px;
+        line-height: 14px;
+        padding:0 12px;
+        &.current{
+          position: relative;
+          background-color: #fff;
+          font-weight: 700;
+          margin-top: -1px;
+          z-index: 10;
+          .text{
+            @include border-none();
+          }
+        }
+        .icon {
+          display: inline-block;
+          width: 12px;
+          height: 12px;
+          vertical-align: top;
+          margin-right: 2px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+          &.decrease {
+            @include bg-image('decrease_3')
+          }
+          &.discount {
+            @include bg-image('discount_3')
+          }
+          &.guarantee {
+            @include bg-image('guarantee_3')
+          }
+          &.invoice {
+            @include bg-image('invoice_3')
+          }
+          &.special {
+            @include bg-image('special_3')
+          }
+        }
+        .text {
+          display: table-cell;
+          width: 56px;
+          vertical-align: middle;
+          @include border-1px(rgba(7, 17, 27, 0.1));
+          font-size: 12px;
+        }
+      }
     }
-    .foods-wrapper{
-
+    .foods-wrapper {
+      flex: 1;
+      .food-list {
+        .title {
+          padding-left: 14px;
+          height: 26px;
+          line-height: 26px;
+          border-left: 2px solid #d9dde1;
+          font-size: 12px;
+          color: rgb(147, 153, 159);
+          background-color: #f3f5f7;
+        }
+        .food-item{
+          display: flex;
+          margin: 18px;
+          padding-bottom: 18px;
+          @include border-1px(rgba(7, 17, 27, 0.1));
+          &:last-child{
+            @include border-none();
+            margin-bottom: 0;
+          }
+          .icon{
+            flex: 0 0 57px;
+            margin-right: 10px;
+          }
+          .content{
+            flex: 1;
+            .name{
+              margin: 2px 0 8px 0;
+              height: 14px;
+              line-height: 14px;
+              font-size: 14px;
+              color: rgb(7,17,27)
+            }
+            .desc,.extra{
+              line-height: 10px;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+            .desc{
+              margin-bottom: 8px;
+              line-height: 12px;
+            }
+            .extra{
+              .count{
+                margin-right:12px;
+              }
+            }
+            .price{
+              font-weight: 700;
+              line-height: 24px;
+              .now{
+                margin-right: 8px;
+                font-size: 14px;
+                color: rgb(240,20,20);
+              }
+              .old{
+                text-decoration: line-through;
+                font-size: 10px;
+                color: rgb(147, 153, 159);
+              }
+            }
+          }
+        }
+      }
     }
   }
 </style>
---
+++ mook-eleme/src/components/shopCart/shopCart.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="shopCart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+        </div>
+        <div class="price">￥0</div>
+        <div class="desc">另需配送费￥{{deliveryPrice}}</div>
+      </div>
+      <div class="content-right">
+        <div class="pay">￥{{minPrice}}元起送</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'shopCart',
+    props: {
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      },
+      selecrFoods: {
+        type: Array,
+        default () {
+          return [];
+        }
+      }
+    },
+    computed: {
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "../../common/styles/icon-style";
+
+  .shopCart {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 50;
+    width: 100%;
+    height: 48px;
+    .content {
+      display: flex;
+      background-color: #141d27;
+      color: rgba(255,255,255,0.4);
+      .content-left {
+        flex: 1;
+        font-size: 0;
+        .logo-wrapper {
+          display: inline-block;
+          position: relative;
+          top: -10px;
+          margin: 0 12px;
+          padding: 6px;
+          width:56px;
+          height:56px;
+          box-sizing: border-box;
+          vertical-align: top;
+          border-radius: 50%;
+          background-color: #141d27;
+          .logo{
+            width:100%;
+            height:100%;
+            border-radius: 50%;
+            background-color: #2b343c;
+            text-align: center;
+            .icon-shopping_cart{
+              font-size: 24px;
+              color: #80858a;
+              line-height:44px;
+            }
+          }
+        }
+        .price {
+          display: inline-block;
+          vertical-align: top;
+          line-height: 24px;
+          margin-top: 12px;
+          padding-right: 12px;
+          box-sizing: border-box;
+          border-right: 1px solid rgba(255,255,255,0.1);
+          font-size: 16px;
+          font-weight: 700;
+        }
+        .desc {
+          display: inline-block;
+          vertical-align: top;
+          margin: 12px 0 0 12px;
+          line-height: 24px;
+          font-size: 10px;
+        }
+      }
+      .content-right {
+        flex: 0 0 105px;
+        width: 105px;
+        background-color: #2b333b;
+        .pay{
+          height:48px;
+          line-height: 48px;
+          text-align: center;
+          font-size: 12px;
+          font-weight: 700;
+        }
+      }
+    }
+  }
+</style>
