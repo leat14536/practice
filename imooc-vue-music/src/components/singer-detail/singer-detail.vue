df2b5fb614636c7879a41940a186a815173e76d6
zltyiu@qq.com <zltyiu@qq.com>
2017-07-04 22:33:51 +0800

    获取歌手数据 歌手详情

--- imooc-vue-music/src/components/singer-detail/singer-detail.vue
+++ imooc-vue-music/src/components/singer-detail/singer-detail.vue
@@ -1,38 +1,68 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script>
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {createSong} from 'common/js/song'
+  import {ERR_OK} from 'api/config'
+  import MusicList from 'components/musiclist/musiclist'
 
   export default {
     name: 'singer-detail',
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created() {
-      console.log(this.singer)
+      this._getDetail()
+    },
+    data() {
+      return {
+        songs: []
+      }
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style lang="scss" scoped>
   @import '../../common/styles/variable.scss';
-
-  .singer-detail {
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: $color-background;
-  }
 
   .slide-enter-active, .slide-leave-active {
     transition: all 0.3s;
