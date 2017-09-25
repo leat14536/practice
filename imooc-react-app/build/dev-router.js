28ed667acd93fae5151f11a7d2072d8925c77492
zltyiu@qq.com <zltyiu@qq.com>
2017-09-25 22:17:39 +0800

    修正拼写错误

--- imooc-react-app/build/dev-router.js
+++ imooc-react-app/build/dev-router.js
@@ -2,10 +2,8 @@
  * Created by Administrator on 2017/9/25 0025.
  */
 const apiRoute = require('express').Router()
-const url = require('url')
-const request = require('request')
 
-const homeAdData = require('../mokeData/homead.json')
+const homeAdData = require('../mockData/homead.json')
 apiRoute.get('/homead', (req, res) => {
   res.json({
     code: 0,
@@ -13,11 +11,11 @@ apiRoute.get('/homead', (req, res) => {
   })
 })
 
-const homeListData = require('../mokeData/homeListData.json')
+const homeListData = require('../mockData/homeListData.json')
 apiRoute.get('/homelist/:city/:page', (req, res) => {
   const params = req.params
-  const paramsCity = params.city
-  const paramsPage = params.page
+  // const paramsCity = params.city
+  // const paramsPage = params.page
 
   res.json({
     code: 0,
